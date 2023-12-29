b01822414cbc79d29b471587b321cdc6c3d1cb77
winston <gumdropsteve@winstons-MacBook-Pro.local>
2023-12-29 15:39:18 -0500

    add trader joe, commented experiment on charts

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ class status (Resource):
 class Both(Resource):
 
     def get(self, todo_id):
+        """
+        return number of visitors and average visit duration 
+        for a given token's website
+        """
+
         if todo_id == 'osmosis':
             data = pd.read_csv(visits_path)
             df = pd.read_csv(avg_visit_path)['average_visit_duration']
@@ -30,18 +35,42 @@ class Both(Resource):
             # clean up the endpoint if necessary
             if (todo_id == 'sushiswap') or (todo_id == 's'):
                 todo_id = 'sushi'
+            elif (todo_id == 'joe') or (todo_id == 'traderjoe'):
+                todo_id = 'traderjoexyz'
             # call the specific token requested
             data = pd.read_csv(visits_path.replace('osmosis', todo_id))
             df = pd.read_csv(avg_visit_path.replace('osmosis', todo_id))['average_visit_duration']
             data = pd.concat([data, df], axis=1)
-        
+            
         data = data.to_dict()
         return {'data': data}, 200
+    
+
+# class Chart(Resource):
+
+#     def get(self, todo_id):
+
+#         if todo_id == 'osmosis':
+#             data = pd.read_csv(visits_path)
+#             df = pd.read_csv(avg_visit_path)['average_visit_duration']
+#             data = pd.concat([data, df], axis=1)
+#         else:
+#             # clean up the endpoint if necessary
+#             if (todo_id == 'sushiswap') or (todo_id == 's'):
+#               todo_id = 'sushi'
+#             # call the specific token requested
+#             data = pd.read_csv(visits_path.replace('osmosis', todo_id))
+#             df = pd.read_csv(avg_visit_path.replace('osmosis', todo_id))['average_visit_duration']
+#             data = pd.concat([data, df], axis=1)
+            
+#         data = data.plot.bar('date', 'visits')
+#         return {'data': data}, 200
 
 
 # add api endpoints
 api.add_resource(status, '/')
 api.add_resource(Both, '/<string:todo_id>')
+# api.add_resource(Chart, '/chart/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run()
